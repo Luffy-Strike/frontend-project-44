99da56bee930d02b67cff51861beabc0ce5102de
Luffy-Strike <ivanros228@gmail.com>
2023-03-16 14:10:25 +0300

    .

---
+++ scr/cli.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+const greeting = (text) => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question("May I have your name? ");
+    console.log(`Hello, ${name}!`);
+    if (text !== undefined)
+        console.log(text);
+    return name;
+}
+
+export default greeting;
